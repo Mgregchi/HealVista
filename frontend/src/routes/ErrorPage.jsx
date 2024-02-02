1cfb739ad105dad47b4ec38998a74ca784218572
abitechcoder <abiolaolalekan39@gmail.com>
2024-02-02 09:52:22 +0100

    Created a react project, added required dependencies and worked on the routes

---
+++ frontend/src/routes/ErrorPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const ErrorPage = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+        <div>
+            <h1>404</h1>
+            <h3>Ooops!!</h3>
+            <p>The page you are looking for doesn't exist or is unavailable</p>
+            <button onClick={() => navigate("/login")} className='py-2 px-4 bg-[#523cdb] rounded-md text-white mt-2'>Go to home</button>
+        </div>
+    </div>
+  )
+}
+
+export default ErrorPage
