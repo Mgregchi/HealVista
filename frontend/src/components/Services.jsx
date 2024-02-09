a1a4223be5214fe1266f4caa32a9850da3774d5f
Adeosun Abiola <abiolaolalekan39@gmail.com>
2024-02-09 16:46:09 +0100

    worked on the services card

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import "../styles/Services.scss";
+import ServicesCards from "./services/ServicesCards";
 
 function Services() {
   return (
-    <div>Services Section</div>
-  )
+    <div className="services">
+      <div className="services__heading">
+        <h5 className="services__heading__title">SERVICES</h5>
+        <h2 className="services__heading__subtitle">
+          TREATMENT AT YOUR FINGERTIPS
+        </h2>
+      </div>
+      <ServicesCards />
+      <div className="services__bg-overlay"></div>
+    </div>
+  );
 }
 
-export default Services
+export default Services;
